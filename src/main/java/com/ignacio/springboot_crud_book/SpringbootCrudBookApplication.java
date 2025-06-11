b34a08cfeffcb547a057dce0cd3a88642782a6ca
Agustin <belcebutesla@gmail.com>
2025-06-11 15:49:55 -0300

    first commit

---
+++ src/main/java/com/ignacio/springboot_crud_book/SpringbootCrudBookApplication.java
@@ -0,0 +1,13 @@
+package com.ignacio.springboot_crud_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignacio/springboot_crud_book/SpringbootCrudBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacio.springboot_crud_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
